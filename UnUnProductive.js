e05418954a5e3117a9cc00fbf114b8c6f22f1bc3
aluminumchassis2 <aluminumchassis@gmail.com>
2021-04-14 17:35:50 -0700

    myspace

--- UnUnProductive.js
+++ UnUnProductive.js
@@ -1,19 +1,34 @@
-function redirect() {
+var ignoreNextRequest = {
+
+};
+
+function redirect(details) {
+    if (details.method != 'GET') {
+        return {};
+    }
+    var timestamp = ignoreNextRequest[details.url];
+	if (timestamp) {
+		delete ignoreNextRequest[details.url];
+		return {};
+	}
+    if(details.documentUrl == "https://myspace.com/") {
+        return {};
+    }
     console.log("request!!")
-    return {redirectUrl: "myspace.com"}
+    ignoreNextRequest[details.documentUrl] = new Date().getTime();
+    chrome.webRequest.onBeforeRequest.removeListener(redirect)
+    setTimeout(listen, 1000)
+    return { redirectUrl: "https://myspace.com/" }
 }
-
-browser.webRequest.onBeforeRequest.addListener(
-    function(info) {
-        console.log(info);
-        return {cancel: true};
-    }, {
-        urls: ['<all_urls>'],
-    },
-    ['blocking']
-);
-
-browser.webRequest.onBeforeRequest.dispatchEvent(new Event("onBeforeRequest"));
+function listen() {
+    browser.webRequest.onBeforeRequest.addListener(
+        redirect, {
+            urls: ["https://*/*", "http://*/*"]
+        },
+        ['blocking']
+    );
+}
+listen()
 
 function receiveMessage(request) {
     if(request.operation == "update") {
